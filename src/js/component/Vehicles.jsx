164294a5e7938d82c2fd0c0318b285060ae84f61
Anais Escalona <115326780+eanais@users.noreply.github.com>
2025-04-04 19:53:38 +0000

    fix Vehicle.jsx

--- src/js/component/Vehicles.jsx
+++ src/js/component/Vehicles.jsx
@@ -1,54 +1,24 @@
-{
-    "vehicles": [
-      {
-        "id": 0,
-        "name": "Sand Crawler",
-        "image": "https://static.wikia.nocookie.net/starwars/images/f/ff/Sandcrawler.png/revision/latest/scale-to-width-down/1000?cb=20130812001443"
-      },
-      {
-        "id": 1,
-        "name": "T-16 skyhopper",
-        "image": "https://static.wikia.nocookie.net/starwars/images/f/f2/T-16_skyhopper_-_SW_20.png/revision/latest/scale-to-width-down/1000?cb=20160805002611"
-      },
-      {
-        "id": 2,
-        "name": "X-34 landspeeder",
-        "image": "https://static.wikia.nocookie.net/starwars/images/b/be/X-34-TGTB.png/revision/latest?cb=20211204185714"
-      },
-      {
-        "id": 3,
-        "name": "TIE/LN starfighter",
-        "image": "https://static.wikia.nocookie.net/starwars/images/9/92/TIEfighter2-Fathead.png/revision/latest?cb=20161109040841"
-      },
-      {
-        "id": 4,
-        "name": "Snowspeeder",
-        "image": "https://static.wikia.nocookie.net/starwars/images/2/27/Rebel_snowspeeder_SWL.png/revision/latest/scale-to-width-down/1000?cb=20250209182658"
-      },
-      {
-        "id": 5,
-        "name": "TIE bomber",
-        "image": "https://static.wikia.nocookie.net/starwars/images/3/3a/TIEBomber-SotGSE.png/revision/latest/scale-to-width-down/1000?cb=20230720013440"
-      },
-      {
-        "id": 6,
-        "name": "AT-AT",
-        "image": "https://static.wikia.nocookie.net/starwars/images/1/16/AT-AT_2_Fathead.png/revision/latest/scale-to-width-down/1000?cb=20161108042721"
-      },
-      {
-        "id": 7,
-        "name": "AT-ST",
-        "image": "https://static.wikia.nocookie.net/starwars/images/f/ff/ATST-SWBdice.png/revision/latest?cb=20230723050455"
-      },
-      {
-        "id": 8,
-        "name": "Storm IV Twin-Pod cloud car",
-        "image": "https://static.wikia.nocookie.net/starwars/images/5/51/Cloud_Car_NEGVV.png/revision/latest/scale-to-width-down/1000?cb=20170410043421"
-      },
-      {
-        "id": 9,
-        "name": "Sail barge",
-        "image": "https://static.wikia.nocookie.net/starwars/images/a/a9/Sailbarge-Chron.png/revision/latest/scale-to-width-down/1000?cb=20230830052225"
-      }
-    ]
-  }
+import React from 'react';
+import vehicleData from "../../img/vehicle.json";
+
+const Vehicle = ({ name, passengers, cargo_capacity, max_atmosphering_speed, id }) => {
+  const vehicleImage = vehicleData?.vehicles?.find(vehicle => vehicle.id === id)?.image || "https://via.placeholder.com/300";
+
+  return (
+    <div className="card" style={{ width: "18rem", flex: "none", margin: "10px" }}>
+      <img src={vehicleImage} className="card-img-top" alt={name} style={{ height: "25rem" }} />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">Passengers: <strong>{passengers}</strong></p>
+        <p className="card-text">Cargo Capacity: <strong>{cargo_capacity}</strong></p>
+        <p className="card-text">Max. Atm. Speed: <strong>{max_atmosphering_speed}</strong></p>
+        <div className='d-flex justify-content-between'>
+          <a href="#" className="btn btn-primary">More Info</a>
+          <button className='btn btn-dark'>Like</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Vehicle;
